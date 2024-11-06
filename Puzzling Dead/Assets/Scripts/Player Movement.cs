1521d8be63f7cee058805ae5190d2c2febd2cd34
s991116 <philip.juhl@gmail.com>
2024-11-06 07:28:03 +0100

    Added player 2, with controlswitch in player-script

--- Puzzling Dead/Assets/Scripts/Player Movement.cs
+++ Puzzling Dead/Assets/Scripts/Player Movement.cs
@@ -4,41 +4,50 @@ public class PlayerMovement : MonoBehaviour
 {
     //Test
     [SerializeField] private float speed;
+
+    [SerializeField] private int playerNumber;
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
+    private int activePlayer;
 
     private void Awake()
     {
         //Grab refernces for rigidbody and animation
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        activePlayer = 0;
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        if(playerNumber == activePlayer) {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
-        //Flips player depending on horizontal input
-        if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(6,6,6);
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-6, 6, 6);
+            //Flips player depending on horizontal input
+            if (horizontalInput > 0.01f)
+                transform.localScale = new Vector3(6,6,6);
+            else if (horizontalInput < -0.01f)
+                transform.localScale = new Vector3(-6, 6, 6);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
+            if (Input.GetKey(KeyCode.Space) && grounded)
+                Jump();
 
-        // set animator parameters
-        anim.SetBool("Walk", horizontalInput != 0);
-        anim.SetBool("Grounded", grounded);
+            // set animator parameters
+            anim.SetBool("Walk", horizontalInput != 0);
+            anim.SetBool("Grounded", grounded);
+        }
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2((body.velocity.x), speed);
-        anim.SetTrigger("jump");
-        grounded = false;
+        if(playerNumber == activePlayer) {
+
+            body.velocity = new Vector2((body.velocity.x), speed);
+            anim.SetTrigger("jump");
+            grounded = false;
+        }
     }private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground") 

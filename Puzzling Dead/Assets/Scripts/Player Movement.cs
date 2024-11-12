5b6036bacf198e27233f8af5fd463aea98256cb9
s991116 <philip.juhl@gmail.com>
2024-11-12 07:30:33 +0100

    Fixed animation when zombie becomes inactive.
    Fixed animation when active zombie is set to inactive while jumping.

--- Puzzling Dead/Assets/Scripts/Player Movement.cs
+++ Puzzling Dead/Assets/Scripts/Player Movement.cs
@@ -20,28 +20,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (playerNumber == GameManager.instance.activePlayerNr)
+        bool playerIsActive = playerNumber == GameManager.instance.activePlayerNr;
+        float horizontalInput = 0;
+        if (playerIsActive)
+            horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Flips player depending on horizontal input, maintaining original scale
+        if (horizontalInput > 0.01f)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-            // Flips player depending on horizontal input, maintaining original scale
-            if (horizontalInput > 0.01f)
-            {
-                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else if (horizontalInput < -0.01f)
-            {
-                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-
-            if (Input.GetKey(KeyCode.Space) && grounded)
-                Jump();
-
-            // Set animator parameters
-            anim.SetBool("Walk", horizontalInput != 0);
-            anim.SetBool("Grounded", grounded);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && grounded && playerIsActive)
+            Jump();
+
+        // Set animator parameters
+        anim.SetBool("Walk", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
+
     }
 
     private void Jump()
